98de612be50d3b8efeb264495357594e45d131e8
Mikołaj Szymański <mikszym95@gmail.com>
2020-04-06 21:50:51 +0200

    Change filter to IN

--- src/main/java/com/yoko/libraryproject/specification/BookDateFilterSpecification.java
+++ src/main/java/com/yoko/libraryproject/specification/BookDateFilterSpecification.java
@@ -1,10 +1,10 @@
 package com.yoko.libraryproject.specification;
 
 import com.yoko.libraryproject.entity.Book;
-import net.kaczmarzyk.spring.data.jpa.domain.Between;
+import net.kaczmarzyk.spring.data.jpa.domain.In;
 import net.kaczmarzyk.spring.data.jpa.web.annotation.Spec;
 import org.springframework.data.jpa.domain.Specification;
 
-@Spec(path = "releaseDate", params = {"releasedAfter", "releasedBefore"}, spec = Between.class)
+@Spec(path = "releaseDate", params = "year", spec = In.class)
 public interface BookDateFilterSpecification extends Specification<Book> {
 }
